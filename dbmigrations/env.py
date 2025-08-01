892347b9729114a4146edf732da926e016c7fbe6
Jardelaste <ibrahim.can1234@gmail.com>
2025-08-01 16:35:47 +0300

    Update env.py

--- dbmigrations/env.py
+++ dbmigrations/env.py
@@ -1,30 +1,23 @@
 from logging.config import fileConfig
-
 from alembic import context
+from app.db import Base
+from app.settings import settings
+from sqlalchemy import create_engine
 
-from app.db import engine, Base
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline() -> None:
-    url = engine.url.render_as_string(hide_password=False)
+    url = settings.DATABASE_URL  # <-- direkt settings'ten al
     context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True,
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
     with context.begin_transaction():
@@ -32,6 +25,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
+    engine = create_engine(settings.DATABASE_URL)  # <-- burayı da güncelle
     with engine.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
         with context.begin_transaction():
